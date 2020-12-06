29c8c07687a27b2d4c24bf79abc654596698e192
Ziyang Xu <ziyangx@princeton.edu>
2020-12-05 23:19:39 -0500

    run remove-bc twice to remove more (first time might create opportunities to merge bbs)

--- transform.sh
+++ transform.sh
@@ -2,4 +2,4 @@
 
 root_path="/u/ziyangx/bounds-check/BoundsCheckExplorer"
 
-opt -load ${root_path}/remove-bc-pass/build/install/lib/CAT.so -remove-bc -dce -simplifycfg $1 -o $2
+opt -load ${root_path}/remove-bc-pass/build/install/lib/CAT.so -simplifycfg -remove-bc -dce -simplifycfg -remove-bc -dce -simplifycfg $1 -o $2
