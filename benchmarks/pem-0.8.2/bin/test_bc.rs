eaebb29daa5ec79b141828d8309d14412078ede8
Natalie Popescu <nataliepopescu@gmail.com>
2020-12-09 05:05:42 -0500

    pem converted

---
+++ benchmarks/pem-0.8.2/bin/test_bc.rs
@@ -0,0 +1,76 @@
+extern crate pem;
+
+use std::time::SystemTime;
+use std::time::Duration;
+use std::io;
+use std::io::Write as IoWrite;
+
+const SAMPLE: &'static str = "-----BEGIN RSA PRIVATE KEY-----\r
+MIIBPQIBAAJBAOsfi5AGYhdRs/x6q5H7kScxA0Kzzqe6WI6gf6+tc6IvKQJo5rQc\r
+dWWSQ0nRGt2hOPDO+35NKhQEjBQxPh/v7n0CAwEAAQJBAOGaBAyuw0ICyENy5NsO\r
+2gkT00AWTSzM9Zns0HedY31yEabkuFvrMCHjscEF7u3Y6PB7An3IzooBHchsFDei\r
+AAECIQD/JahddzR5K3A6rzTidmAf1PBtqi7296EnWv8WvpfAAQIhAOvowIXZI4Un\r
+DXjgZ9ekuUjZN+GUQRAVlkEEohGLVy59AiEA90VtqDdQuWWpvJX0cM08V10tLXrT\r
+TTGsEtITid1ogAECIQDAaFl90ZgS5cMrL3wCeatVKzVUmuJmB/VAmlLFFGzK0QIh\r
+ANJGc7AFk4fyFD/OezhwGHbWmo/S+bfeAiIh2Ss2FxKJ\r
+-----END RSA PRIVATE KEY-----\r
+\r
+-----BEGIN RSA PUBLIC KEY-----\r
+MIIBOgIBAAJBAMIeCnn9G/7g2Z6J+qHOE2XCLLuPoh5NHTO2Fm+PbzBvafBo0oYo\r
+QVVy7frzxmOqx6iIZBxTyfAQqBPO3Br59BMCAwEAAQJAX+PjHPuxdqiwF6blTkS0\r
+RFI1MrnzRbCmOkM6tgVO0cd6r5Z4bDGLusH9yjI9iI84gPRjK0AzymXFmBGuREHI\r
+sQIhAPKf4pp+Prvutgq2ayygleZChBr1DC4XnnufBNtaswyvAiEAzNGVKgNvzuhk\r
+ijoUXIDruJQEGFGvZTsi1D2RehXiT90CIQC4HOQUYKCydB7oWi1SHDokFW2yFyo6\r
+/+lf3fgNjPI6OQIgUPmTFXciXxT1msh3gFLf3qt2Kv8wbr9Ad9SXjULVpGkCIB+g\r
+RzHX0lkJl9Stshd/7Gbt65/QYq+v+xvAeT0CoyIg\r
+-----END RSA PUBLIC KEY-----\r
+";
+
+fn now() -> SystemTime {
+    return SystemTime::now();
+}
+
+fn elapsed(start: SystemTime) -> (Duration, bool) {
+    match start.elapsed() {
+        Ok(delta) => return (delta, false),
+        _ => return (Duration::new(0, 0), true),
+    }
+}
+
+fn bench_test(n_iter: usize) {
+    for _ in 0..n_iter {
+        pem::parse_many(SAMPLE);
+    }
+}
+
+#[no_mangle]
+#[inline(never)]
+fn bench() {
+    // setup
+
+    let start = now();
+    let mut timing_error: bool = false;
+    let n_iterations: usize = 7000;
+
+    // bench
+    bench_test(n_iterations);
+
+    let (total, err) = elapsed(start);
+    if err {
+        timing_error = true;
+    }
+
+    if timing_error {
+        let _r = writeln!(&mut io::stderr(), "{:}", "Timing error");
+    } else {
+        writeln!(&mut io::stderr(), "{:} {:} {:}.{:09}",
+        n_iterations as u64,
+        "Iterations; Time",
+        total.as_secs(),
+        total.subsec_nanos());
+    }
+}
+
+fn main() {
+    bench();
+}
