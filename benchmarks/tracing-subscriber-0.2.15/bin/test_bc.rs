891095d81b847204ed28a194148590afaa3c1d1e
Natalie Popescu <nataliepopescu@gmail.com>
2020-12-08 23:42:17 -0500

    didn't try enough iter; visibly runs now

--- benchmarks/tracing-subscriber-0.2.15/bin/test_bc.rs
+++ benchmarks/tracing-subscriber-0.2.15/bin/test_bc.rs
@@ -1,12 +1,27 @@
 //#![feature(test)]
 
-extern crate tracing;
-extern crate tracing_subscriber;
-extern crate bencher;
+//extern crate tracing;
+//extern crate tracing_subscriber;
+//extern crate bencher;
 
-use bencher::black_box;
+use criterion::black_box;
 use tracing::{span, Event, Id, Metadata};
 use tracing_subscriber::{prelude::*, EnvFilter};
+use std::time::SystemTime;
+use std::time::Duration;
+use std::io::Write as IoWrite;
+use std::io;
+
+fn now() -> SystemTime {
+    return SystemTime::now();
+}
+
+fn elapsed(start: SystemTime) -> (Duration, bool) {
+    match start.elapsed() {
+        Ok(delta) => return (delta, false),
+        _ => return (Duration::new(0, 0), true),
+    }
+}
 
 struct EnabledSubscriber;
 
@@ -48,11 +63,31 @@ fn bench() {
     let filter = "static_filter=info"
         .parse::<EnvFilter>()
         .expect("should parse");
+
+    let start= now();
+    let mut timing_error: bool = false;
+    let n_iterations: usize = 150000000;
+
     black_box(tracing::subscriber::with_default(EnabledSubscriber.with(filter), || {
-        for _ in 0..50000 {
+        for _ in 0..n_iterations{
             black_box(tracing::debug!(target: "static_filter", "hi"));
         }
-    }))
+    }));
+
+    let (total, err) = elapsed(start);
+    if err {
+        timing_error = true;
+    }
+
+    if timing_error {
+        let _r = writeln!(&mut io::stderr(), "{:}", "Timing error");
+    } else {
+        writeln!(&mut io::stderr(), "{:} {:} {:}.{:09}",
+        "Iterations; Time",
+        n_iterations as u64,
+        total.as_secs(),
+        total.subsec_nanos());
+    }
 }
 
 fn main() {
