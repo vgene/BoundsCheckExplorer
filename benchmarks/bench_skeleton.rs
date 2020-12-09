7a29231402fd6a512d2f9ba6e72cb411dc5d7653
Natalie Popescu <nataliepopescu@gmail.com>
2020-12-09 00:00:36 -0500

    helpful boilerplate

---
+++ benchmarks/bench_skeleton.rs
@@ -0,0 +1,46 @@
+use std::time::SystemTime;
+use std::time::Duration;
+use std::io;
+use std::io::Write as IoWrite;
+
+fn now() -> SystemTime {
+    return SystemTime::now();
+}
+
+fn elapsed(start: SystemTime) -> (Duration, bool) {
+    match start.elapsed() {
+        Ok(delta) => return (delta, false),
+        _ => return (Duration::new(0, 0), true),
+    }
+}
+
+#[no_mangle]
+#[inline(never)]
+fn bench() {
+    // setup
+
+    let start = now();
+    let mut timing_error: bool = false;
+    let n_iterations: usize = 700;
+
+    // bench
+
+    let (total, err) = elapsed(start);
+    if err {
+        timing_error = true;
+    }
+
+    if timing_error {
+        let _r = writeln!(&mut io::stderr(), "{:}", "Timing error");
+    } else {
+        writeln!(&mut io::stderr(), "{:} {:} {:}.{:09}",
+        "Iterations; Time",
+        n_iterations as u64,
+        total.as_secs(),
+        total.subsec_nanos());
+    }
+}
+
+fn main() {
+    bench();
+}
