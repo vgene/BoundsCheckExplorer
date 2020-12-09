f453a92586040792f50b1f68c6bb92f03454f25f
Natalie Popescu <nataliepopescu@gmail.com>
2020-12-09 02:02:59 -0500

    update boilerplate

--- benchmarks/bench_skeleton.rs
+++ benchmarks/bench_skeleton.rs
@@ -14,6 +14,12 @@ fn elapsed(start: SystemTime) -> (Duration, bool) {
     }
 }
 
+fn bench_test(n_iter: usize) {
+    for _ in 0..n_iter {
+
+    }
+}
+
 #[no_mangle]
 #[inline(never)]
 fn bench() {
@@ -21,9 +27,10 @@ fn bench() {
 
     let start = now();
     let mut timing_error: bool = false;
-    let n_iterations: usize = 700;
+    let n_iterations: usize = 100000;
 
     // bench
+    bench_test(n_iterations);
 
     let (total, err) = elapsed(start);
     if err {
@@ -34,8 +41,8 @@ fn bench() {
         let _r = writeln!(&mut io::stderr(), "{:}", "Timing error");
     } else {
         writeln!(&mut io::stderr(), "{:} {:} {:}.{:09}",
-        "Iterations; Time",
         n_iterations as u64,
+        "Iterations; Time",
         total.as_secs(),
         total.subsec_nanos());
     }
