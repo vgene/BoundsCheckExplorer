fd8c715d9c01143bdd9346333ab2bb226851103b
Natalie Popescu <nataliepopescu@gmail.com>
2020-12-08 23:46:57 -0500

    add timer to assume_true benchmark

--- benchmarks/assume_true/bin/test_bc.rs
+++ benchmarks/assume_true/bin/test_bc.rs
@@ -1,6 +1,22 @@
 extern crate forpaper;
 extern crate rand;
 
+use std::time::SystemTime;
+use std::time::Duration;
+use std::io;
+use std::io::Write as IoWrite;
+
+fn now() -> SystemTime {
+    return SystemTime::now();
+}
+
+fn elapsed(start: SystemTime) -> (Duration, bool) {
+    match start.elapsed() {
+        Ok(delta) => return (delta, false),
+        _ => return (Duration::new(0, 0), true),
+    }
+}
+
 #[no_mangle]
 #[inline(never)]
 fn unknown_size_bench() {
@@ -10,8 +26,27 @@ fn unknown_size_bench() {
         other_buf[i] = rand::random();
     }
 
-    for _ in 0..400 {
+    let start = now();
+    let mut timing_error: bool = false;
+    let n_iterations: usize = 700;
+
+    for _ in 0..n_iterations {
         forpaper::unknown_size(&other_buf, &mut app_buf);
+    }
+
+    let (total, err) = elapsed(start);
+    if err {
+        timing_error = true;
+    }
+
+    if timing_error {
+        let _r = writeln!(&mut io::stderr(), "{:}", "Timing error");
+    } else {
+        writeln!(&mut io::stderr(), "{:} {:} {:}.{:09}",
+        "Iterations; Time",
+        n_iterations as u64,
+        total.as_secs(),
+        total.subsec_nanos());
     }
 }
 
