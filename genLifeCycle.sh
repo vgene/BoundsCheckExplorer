944817a30a5c9124a1ef3cff11e1883394802ac3
Ziyang Xu <ziyangx@princeton.edu>
2021-03-18 17:22:27 -0400

    add tagger into script

--- genLifeCycle.sh
+++ genLifeCycle.sh
@@ -4,33 +4,42 @@
 script_path=`realpath $0`
 root_path=`dirname $script_path`
 
-# Statically link all the rlib and outerloop prof RT
-# The inline threshold is the same as Rustc
-
-rlib_path=`rustc --print target-libdir`
-
-Input=$1
+Original=$1
 MetaO0="meta-o0"
 MetaO0Prof="meta-o0-with-prof"
 MetaO3="meta-o3"
 MetaO3Prof="meta-o3-with-prof"
 
+# Tag each GEP and Branch pair with the unique metadata
+echo "Tagging GEP and Branch pairs"
+opt -load $root_path/bc-life-cycle-pass/build/install/lib/CAT.so $Original -mark-bc-gep -o $MetaO0.bc
+
+llvm-dis $MetaO0.bc
+
+# Optimize with opt -O3
+echo "Optimizing using opt -O3"
+opt -O3 $MetaO0.bc -inline-threshold=275 -o $MetaO3.bc 
+
+# Statically link all the rlib and outerloop prof RT
+# The inline threshold is the same as Rustc
+rlib_path=`rustc --print target-libdir`
+
 genEdgeProf() {
-    echo "Generating $2"
+    echo "Generating Edge Prof for $2"
     local in=$1
     local out=$2
     # Add Edge Prof 
     opt -pgo-instr-gen -instrprof $in.bc -o $in-prof.bc
-    clang -O3 $in-prof.bc $rlib_path/*.rlib $rlib_path/*.rlib -fprofile-generate -lstdc++ -ldl -lpthread -lc -lm -o $in-prof.exe -mllvm -inline-threshold=275
+    clang -O3 $in-prof.bc $rlib_path/*.rlib $rlib_path/*.rlib -fprofile-generate -lstdc++ -ldl -lpthread -lc -lm -o $in-prof.exe
     LLVM_PROFILE_FILE=edge.profraw ./$in-prof.exe
     llvm-profdata merge edge.profraw -output=edgeprof.out
     opt -block-freq -pgo-instr-use -pgo-test-profile-file=./edgeprof.out $in.bc -o $out.bc 
 }
 
-cp $Input $MetaO0.bc
-opt -O3 $MetaO0.bc -o $MetaO3.bc
 genEdgeProf $MetaO0 $MetaO0Prof
 genEdgeProf $MetaO3 $MetaO3Prof
 
 llvm-dis $MetaO0Prof.bc
 llvm-dis $MetaO3Prof.bc
+
+
