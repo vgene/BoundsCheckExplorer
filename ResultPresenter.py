d40a1be778d760a3208df1da5c89bbdf244d8e78
Ziyang Xu <ziyangx@cs.princeton.edu>
2021-04-10 04:30:35 +0000

    change port

--- ResultPresenter.py
+++ ResultPresenter.py
@@ -511,4 +511,4 @@ if __name__ == '__main__':
             html.Div(id='page-content')
         ])
 
-        app.run_server(debug=False, host='0.0.0.0', port=8070)
+        app.run_server(debug=False, host='0.0.0.0', port=8090)
